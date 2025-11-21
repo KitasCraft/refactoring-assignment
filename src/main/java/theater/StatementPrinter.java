346bf4b9b1a11693d51d0fc4edba79d56ef87e3a
KitasCraft <75518491+KitasCraft@users.noreply.github.com>
2025-11-20 20:49:12 -0500

    Completed Task 2.3: Removing frmt variable

--- src/main/java/theater/StatementPrinter.java
+++ src/main/java/theater/StatementPrinter.java
@@ -27,24 +27,23 @@ public class StatementPrinter {
         final StringBuilder result = new
                 StringBuilder("Statement for " + invoice.getCustomer() + System.lineSeparator());
 
-        final NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
-
-        for (Performance performance : invoice.getPerformances()) {
-
-            final int thisAmount = getAmount(performance);
-
+        for (Performance p : invoice.getPerformances()) {
             // add volume credits
-            volumeCredits += getVolumeCredits(performance);
+            volumeCredits += getVolumeCredits(p);
 
             // print line for this order
-            result.append(String.format("  %s: %s (%s seats)%n", getPlay(performance).getName(),
-                    frmt.format(thisAmount / Constants.PERCENT_FACTOR), performance.getAudience()));
-            totalAmount += thisAmount;
+            result.append(String.format("  %s: %s (%s seats)%n", getPlay(p).getName(),
+                    usd(getAmount(p)), p.getAudience()));
+            totalAmount += getAmount(p);
         }
         result.append(String.format("Amount owed is %s%n",
-                frmt.format(totalAmount / Constants.PERCENT_FACTOR)));
+                usd(totalAmount)));
         result.append(String.format("You earned %s credits%n", volumeCredits));
         return result.toString();
+    }
+
+    private static String usd(int totalAmount) {
+        return NumberFormat.getCurrencyInstance(Locale.US).format(totalAmount / Constants.PERCENT_FACTOR);
     }
 
     private int getVolumeCredits(Performance performance) {
